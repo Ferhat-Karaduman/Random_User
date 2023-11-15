a7233c4ffbc3b6c4d5aab0684a9a09e470e0f6e1
Ferhat-Karaduman <ferhatkaraduman46@gmail.com>
2023-11-15 13:59:14 +0100

    js finish

--- app.js
+++ app.js
@@ -17,3 +17,27 @@ function getPerson(cb) {
   
     request.send();
   }
+
+  function getData(response, cb) {
+    const data = JSON.parse(response);
+  
+    const {
+      name: { first },
+      name: { last },
+      picture: { large },
+      location: { street },
+      phone,
+      email,
+    } = data.results[0];
+    cb(first, last, large, street, phone, email);
+  }
+  
+  function showData(first, last, large, street, phone, email) {
+    document.getElementById("name").textContent = `${first} ${last}`;
+    document.getElementById("first").textContent = first;
+    document.getElementById("last").textContent = last;
+    document.getElementById("street").textContent = street.name;
+    document.getElementById("phone").textContent = phone;
+    document.getElementById("email").textContent = email;
+    document.getElementById("photo").src = large;
+  }
