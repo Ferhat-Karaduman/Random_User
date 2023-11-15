c6f9ea2a754a8c9de54ab91933ba77576b936879
Ferhat-Karaduman <ferhatkaraduman46@gmail.com>
2023-11-15 13:58:22 +0100

    api added

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ const btn = document.getElementById("btn");
 btn.addEventListener("click", function () {
   getPerson(getData);
 });
+
+function getPerson(cb) {
+    const url = "https://randomuser.me/api/";
+    const request = new XMLHttpRequest();
+  
+    request.open("GET", url, true);
+    request.onload = function () {
+      if (this.status === 200) {
+        cb(this.responseText, showData);
+      }
+    };
+  
+    request.send();
+  }
